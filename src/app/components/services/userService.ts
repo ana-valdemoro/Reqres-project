6a04b9e1bce4d3b6b753568ca2a623b273d72088
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-10-01 21:12:00 +0100

    Query params addded to route

---
+++ src/app/components/services/userService.ts
@@ -0,0 +1,50 @@
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "src/app/models/User";
+import { UserProvider } from "src/app/providers/user.provider";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private userList$ = new BehaviorSubject<User[]>([]);
+    private users: User[] = [];
+
+    constructor(private userProvider: UserProvider) {}
+
+    setUserList() {
+        this.userList$.next(this.users);
+    }
+
+    getUsersList(): Observable<User[]> {
+        return this.userList$.asObservable();
+    }
+
+    async requestUserBatch(page:string):Promise<any>{
+        return this.userProvider.getUsersPerPage(page)
+            .then(res =>{
+                this.users = res.data;
+                this.setUserList();
+                return res;
+            });
+    }
+ 
+    deleteUserFromList(id: number): void {
+        this.userProvider.deleteUser(id)
+            .then(() => {
+                this.users = this.users.filter((user) => user.id !== id);
+                this.setUserList();
+            });
+    }
+
+    createUser(user: unknown):void {
+        this.userProvider.createUser(user)
+            .then(user => {
+                this.users.push(user);
+                this.setUserList();
+            });
+    }
+
+}
--- src/app/components/user-list-component/user-list-component.component.ts
+++ src/app/components/user-list-component/user-list-component.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/User';
-import { UserService } from 'src/app/services/userService';
+import { UserService } from 'src/app/components/services/userService';
 
 @Component({
   selector: 'app-user-list-component',
@@ -11,13 +12,21 @@ import { UserService } from 'src/app/services/userService';
 export class UserListComponentComponent implements OnInit {
   users: User[] = [];
   userSubscription: Subscription | undefined;
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
   pageSize: number = 6;
-  page: number = 1 ;
+  page: number = 1;
   total: number = 12;
   ngOnInit(): void {
+    this.checkForQueryParams();
     this.subscribeToUserList();
     this.getUserPerPage();
+  }
+
+  checkForQueryParams() {
+    this.route.queryParamMap.subscribe(params => {
+      let page = params.get('page');
+      if (page !== null) this.page = parseInt(page);
+    });
   }
   getUserPerPage() {
     this.userService.requestUserBatch(this.page.toString())
@@ -40,6 +49,10 @@ export class UserListComponentComponent implements OnInit {
   tabSize(event: number) {
     this.page = event;
     this.getUserPerPage();
+    this.changeQuery();
+  }
+  changeQuery() {
+    this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: this.page }});
   }
 
 
