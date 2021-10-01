4ae4a6caadfd500382d42e0a340bb0d001b22ff3
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-10-01 11:05:30 +0100

    New user component added app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserListComponentComponent } from './components/user-list-component/user-list-component.component';
+import { NewUserComponentComponent } from './components/new-user-component/new-user-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserListComponentComponent
+    UserListComponentComponent,
+    NewUserComponentComponent
   ],
   imports: [
     BrowserModule,
