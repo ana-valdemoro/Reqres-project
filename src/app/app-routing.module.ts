ea998cdbe0b286c7cf733ef48e7810084a45ff38
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-10-01 11:04:16 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponentComponent } from './components/new-user-component/new-user-component.component';
+import { UserListComponentComponent } from './components/user-list-component/user-list-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "users", pathMatch: 'full' },
+  {path: "users", component: UserListComponentComponent},
+  {path: "newUser", component: NewUserComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
