159c9d4aad9e10a605567f9981231a8ecbd58be8
Ana Valdemoro <ana_v_r97@hotmail.com>
2021-10-03 20:34:32 +0100

    Add select to choose number of items to show in List and added size query params to route

--- src/app/components/user-list-component/user-list-component.component.ts
+++ src/app/components/user-list-component/user-list-component.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/User';
-import { UserService } from 'src/app/components/services/userService';
+import { UserService } from 'src/app/services/userService';
 
 @Component({
   selector: 'app-user-list-component',
@@ -13,47 +13,65 @@ export class UserListComponentComponent implements OnInit {
   users: User[] = [];
   userSubscription: Subscription | undefined;
   constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
-  pageSize: number = 6;
+  sizePerpage: number = 6;
+  pageSizes: number [] = [4 , 6, 8, 12];
   page: number = 1;
   total: number = 12;
+  sizeQueryParamsIsActive : boolean = false;
   ngOnInit(): void {
     this.checkForQueryParams();
     this.subscribeToUserList();
     this.getUserPerPage();
   }
 
-  checkForQueryParams() {
+  checkForQueryParams():void {
     this.route.queryParamMap.subscribe(params => {
       let page = params.get('page');
-      if (page !== null) this.page = parseInt(page);
+      let size = params.get('size');
+      if(page !== null) this.page = parseInt(page);
+      if(size !== null) {
+        this.sizePerpage = parseInt(size);
+        this.sizeQueryParamsIsActive = true;
+      }else{
+        this.sizeQueryParamsIsActive = false;
+      } 
     });
   }
-  getUserPerPage() {
-    this.userService.requestUserBatch(this.page.toString())
+  getUserPerPage():void {
+    this.userService.requestUserBatch(this.page.toString(), this.sizeQueryParamsIsActive ? this.sizePerpage.toString() : undefined)
       .then(res => {
-        this.pageSize = res.per_page;
+        this.sizePerpage = res.per_page;
         this.page = res.page;
         this.total = res.total;
       });
   }
-  subscribeToUserList() {
+  subscribeToUserList():void {
     this.userSubscription = this.userService.getUsersList().subscribe(users => {
       this.users = users;
     })
 
   }
 
-  requestUserRemoval(id: number) {
+  requestUserRemoval(id: number):void {
     this.userService.deleteUserFromList(id);
   }
-  tabSize(event: number) {
-    this.page = event;
+  ChangePage(nextPage: number):void {
+    this.page = nextPage;
     this.getUserPerPage();
-    this.changeQuery();
+    this.changeQueryParams();
   }
-  changeQuery() {
-    this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: this.page }});
+  changeQueryParams() {
+    if(this.sizeQueryParamsIsActive){
+      this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: this.page, size: this.sizePerpage  }});
+    }else{
+      this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: this.page }});
+    }
   }
 
-
+  changePageSize(): void{
+    this.page = 1;
+    this.getUserPerPage();
+    this.router.navigate(['.'], { relativeTo: this.route, queryParams: { page: this.page, size: this.sizePerpage  }});
+  }
+  
 }
